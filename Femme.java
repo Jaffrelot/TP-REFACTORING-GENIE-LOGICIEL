4f054100d059dde37c418f941d6571bdf6adeb9d
Jaffrelot <andriatsitohainarivo7@gmail.com>
2024-04-01 15:44:04 +0300

    deplacer attribut classe femme dans le parent

--- Femme.java
+++ Femme.java
@@ -1,13 +1,5 @@
 package iut.bad;
 
 public class Femme extends Humain{
-	private String nom;
-	private String prenom;
-	private int age;
-	public Femme(String nom, String prenom, int age) {
-		this.nom = nom;
-		this.prenom = prenom;
-		this.age = age;
-	}
 	
 }
