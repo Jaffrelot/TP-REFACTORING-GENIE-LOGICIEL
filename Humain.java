113cb55a35cf73cb036d3d6a4036b4c48c592db0
Jaffrelot <andriatsitohainarivo7@gmail.com>
2024-04-01 15:32:41 +0300

    creation classe Humain.java

---
+++ Humain.java
@@ -0,0 +1,13 @@
+package iut.bad;
+
+public class Humain{
+	private String nom;
+	private String prenom;
+	private int age;
+	public Humain(String nom, String prenom, int age) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+	}
+
+}
