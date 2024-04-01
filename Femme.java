53638935ac9488c70005b5adfa704cb1f7ceac3b
Jaffrelot <andriatsitohainarivo7@gmail.com>
2024-04-01 15:58:02 +0300

    creation methode main instancie homme et femme

--- Femme.java
+++ Femme.java
@@ -1,5 +1,13 @@
 package iut.bad;
 
 public class Femme extends Humain{
-	
+	public Femme(String nom, String prenom,int age){
+		super(nom,prenom,age);
+	}
+	public static void main(String[] args) {
+        Homme homme = new Homme("Jean", "Dupont", 30);
+        Femme femme = new Femme("Marie", "Dubois", 25);
+
+        femme.ami(homme); // Amiti� par d�faut de 100 jours
+    }
 }
